b02e9c6d91059c85742af2c173307d72d8913a63
poiNt3D <point4d@gmail.com>
2013-07-19 19:08:54 -0700

    Update index.php

--- Minecraft.Resources/index.php
+++ Minecraft.Resources/index.php
@@ -1,6 +1,6 @@
 <?php
-  ini_set('display_errors', 1);
-  error_reporting(E_ALL);
+  //ini_set('display_errors', 1);
+  //error_reporting(E_ALL);
 $directory = dirname ( __FILE__ );
 
     function directoryToArray($directory) {
